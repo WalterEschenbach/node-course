a1cdc727ccae8ff36ce11d8b3ad44be7bb79cad0
Walter <eschenbach.walter@gmail.com>
2020-02-26 15:10:02 -0500

    web-server update

---
+++ web-server/src/app.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Walter"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Walter"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Section",
+    message: "This is some helpful text...",
+    name: "Walter"
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Chicago"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
